d111dd52e886c0a08553ff179d609f01f99fc87e
shantalrohbi <shantal.rohbi@student.moringaschool.com>
2024-10-18 12:07:30 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,30 +1,42 @@
-from flask import Flask, request, session, jsonify, make_response
+
+#!/usr/bin/env python3
+
+from flask import Flask, make_response, jsonify, session
+from flask_migrate import Migrate
+
+from models import db, Article
 
 app = Flask(__name__)
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
+migrate = Migrate(app, db)
 
-@app.route('/sessions/<string:key>', methods=['GET'])
-def show_session(key):
+db.init_app(app)
 
-    session["hello"] = session.get("hello") or "World"
-    session["goodnight"] = session.get("goodnight") or "Moon"
+@app.route('/clear')
+def clear_session():
+    session['page_views'] = 0
 
-    response = make_response(jsonify({
-        'session': {
-            'session_key': key,
-            'session_value': session[key],
-            'session_accessed': session.accessed,
-        },
-        'cookies': [{cookie: request.cookies[cookie]}
-            for cookie in request.cookies],
-    }), 200)
+    return {'message': '200: Successfully cleared session data'}, 200
 
-    response.set_cookie('mouse', 'Cookie')
+@app.route('/articles')
+def index_articles():
+    articles = [article.to_dict() for article in Article.query.all()]
+    return make_response(jsonify(articles), 200)
 
-    return response
+@app.route('/articles/<int:id>')
+def show_article(id):
 
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
+
+    if session['page_views'] <= 3:
+        return Article.query.filter(Article.id == id).first().to_dict(), 200
+        
+    return {'message': 'Maximum pageview limit reached'}, 401
+    
 if __name__ == '__main__':
     app.run(port=5555)
-    
